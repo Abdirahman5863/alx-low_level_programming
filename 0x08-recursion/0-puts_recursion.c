a3988bea12f9bb474545a8c14e0181385cde67a9
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-01-10 08:19:42 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts_recursion - function like puts();
  * @s: input
@@ -6,13 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-
-	else
-		_putchar('\n');
+    if (*s)
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    else
+    {
+        _putchar('\n');
+    }
 }
  
