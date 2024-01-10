d61f5b58ab1075bcc435ebb205bf7ef82b6931f5
Abdirahman5863 <abdirahmanabdi5863@gmail.com>
2024-01-10 09:45:04 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- *_puts_recursion - function name
- *@s: parameter of the function
+ * _puts_recursion - prints a string
+ * @s: a pointer to a string / stdin (0)
+ * Return: Always 0 (success)
  */
 void _puts_recursion(char *s)
 {
